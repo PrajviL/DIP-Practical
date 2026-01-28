caad05df875ea9541ee5686bd64fb0ca97495573
PrajviL <prajvilohakare@gmail.com>
2026-01-28 20:51:05 +0530

    Update Practical1.m

--- Practical1.m
+++ Practical1.m
@@ -16,12 +16,12 @@ figure;
 
 subplot(1,3,1);
 imshow(img);
-title('Original Image");
+title('Original Image');
 
 subplot(1,3,2);
 imshow(dark_img);
-title('Darkened Image");
+title('Darkened Image');
 
 subplot(1,3,3);
 imshow(bright_img);
-title('Brightened Image");
+title('Brightened Image');
